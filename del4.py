e1de9d2fbe07cb317b4f15d2cfb279efd0da0804
oleboifsh <olesia.boiarchuk@elevmail.stockholm.se>
2025-01-28 23:08:48 +0100

    del4 - added more paterns

--- del4.py
+++ del4.py
@@ -32,6 +32,44 @@ for i in range(6):
 print("Version e: ")
 string_original = "....#..."
 for x in range(0, 6):
-    str_p2 = string_original[(6 - x):]
     str_p1 = string_original[:(5 - x)] + "#"
+    str_p2 = string_original[(6 - x):]
     print(str_p1 + str_p2)
+
+#f:
+print("Version f: ")
+for cur in range(0, 6):
+    str = ""
+    for x in range(0, 7):
+        if x == cur or cur == 5-x:
+            str += "#"
+        else:
+            str += "."
+    print(str+".")
+
+#g:
+print("Version g: ")
+for cur in range(0, 6):
+    str = ""
+    for x in range(0, 7):
+        if x %2 == 0:
+            str += "#"
+        else:
+            str += "."
+    print(str+".")
+
+
+
+#j:
+print("Version j: ")
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x%3 == y%3:
+            s += "."
+        elif x%3 == (y + 1)%3:
+            s += "#"
+        else:
+            s += "O"
+    print(s)
+
